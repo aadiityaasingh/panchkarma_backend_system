f1e30bbb4c043f0dad7a7c9f30c88bcdde35b82a
aadiityaasingh <adityasingh.6488@gmail.com>
2026-02-26 00:49:17 +0530

    added middlewares

--- src/routes/session.route.js
+++ src/routes/session.route.js
@@ -4,11 +4,16 @@ const {
   getSessionsPlan, updateSessionStatus
 } = require("../controllers/session.controller.js");
 
+const {protect} = require("../middlewares/auth.js");
+const authorize = require("../middlewares/authorize.js");
+
 const router = express.Router();
 
 router.get("/plan/:planId", getSessionsPlan);
 router.patch(
   "/:sessionId/status",
+  protect,
+  authorize("therapist"),
   updateSessionStatus
 );
 

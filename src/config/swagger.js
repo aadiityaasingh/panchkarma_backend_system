a6d0012b81a464fd94d284b53d7739e3f0b44240
aadiityaasingh <adityasingh.6488@gmail.com>
2026-03-06 00:57:32 +0530

    swagger UI functions

---
+++ src/config/swagger.js
@@ -0,0 +1,41 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Panchakarma Management API",
+      version: "1.0.0",
+      description: "Backend API documentation for Panchakarma Management System"
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      }
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT"
+        }
+      }
+    },
+
+    security: [
+      {
+        bearerAuth: []
+      }
+    ]
+  },
+  apis: [
+    "src/routes/*.js",
+    "src/controllers/*.js",
+    "src/app.js"
+  ]
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;

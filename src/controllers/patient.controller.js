15f7110b84679efda2b3157b0874008e92fcb149
aadiityaasingh <adityasingh.6488@gmail.com>
2026-03-05 20:55:08 +0530

    patient history added

--- src/controllers/patient.controller.js
+++ src/controllers/patient.controller.js
@@ -1,4 +1,7 @@
 const patientModel = require("../models/patient.model.js");
+const therapyPlanModel = require("../models/therapyPlan.model.js");
+const sessionModel = require("../models/session.model.js");
+const billModel = require("../models/bill.model.js");
 
 async function createPatient(req, res) {
   try {
@@ -40,7 +43,44 @@ async function getPatients(req, res) {
   }
 }
 
+async function getPatientHistory(req, res){
+  try {
+    const { patientId } = req.params;
+
+    const plans = await therapyPlanModel.find({ patient: patientId })
+      .populate("therapies.therapy", "name category")
+      .sort({ startDate: -1 });
+
+    const planIds = plans.map(p => p._id);
+
+    const sessions = await sessionModel.find({
+      therapyPlan: { $in: planIds }
+    }).populate("therapy", "name");
+
+    const bills = await billModel.find({
+      therapyPlan: { $in: planIds }
+    });
+
+    res.status(200).json({
+      success: true,
+      data: {
+        plans,
+        sessions,
+        bills
+      }
+    });
+  } catch (error) {
+    console.error("PATIENT HISTORY ERROR:", error);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch patient history"
+    });
+  }
+}
+
 module.exports = {
   createPatient,
   getPatients,
+  getPatientHistory
 };

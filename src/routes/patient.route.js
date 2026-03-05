0a12d498a77b49ffa8373a226b4a261f045b6208
aadiityaasingh <adityasingh.6488@gmail.com>
2026-03-05 20:53:51 +0530

    patient history route

--- src/routes/patient.route.js
+++ src/routes/patient.route.js
@@ -2,15 +2,14 @@ const express = require("express");
 
 const {
   createPatient,
-  getPatients
+  getPatients,
+  getPatientHistory
 } = require("../controllers/patient.controller.js");
-
 const {
   createPatientValidation
 } = require("../validation/patient.validation.js");
-
 const validate = require("../middlewares/validate.js");
-
+const {protect} = require("../middlewares/auth.js");
 const router = express.Router();
 
 router.post(
@@ -19,7 +18,11 @@ router.post(
   validate,
   createPatient
 );
-
 router.get("/", getPatients);
+router.get(
+  "/:patientId/history",
+  protect,
+  getPatientHistory
+);
 
 module.exports = router;

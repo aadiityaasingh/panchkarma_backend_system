771fa95c26dcfd9866a0a8dcb2dfcaf21f335fca
aadiityaasingh <adityasingh.6488@gmail.com>
2026-03-07 01:32:05 +0530

    updated url in swagger

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000"
+        url: "https://panchakarma-backend-system.onrender.com"
       }
     ],
     components: {

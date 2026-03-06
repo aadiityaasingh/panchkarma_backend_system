d31d5cc95730e1f6d9e5ec810117929180cf5193
aadiityaasingh <adityasingh.6488@gmail.com>
2026-03-07 01:09:11 +0530

    updation

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+require("dotenv").config();
+
 const app = require("./src/app.js");
 const connectDB = require("./src/config/db.js");
-require("dotenv").config();
 
 connectDB();
 
 app.listen(process.env.PORT, () => {
     console.log(`server is running on this ${process.env.PORT} port`);
 });
-
